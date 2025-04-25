621ed3d959c47188526842fcb9bc1b7e5188a2b2
Efe Costu <efecostu@Efes-MacBook-Air.local>
2025-04-25 17:56:06 +0300

    band.png - publicall

--- src/app/components/Card.tsx
+++ src/app/components/Card.tsx
@@ -167,10 +167,7 @@ function Band({
   const gltf = useGLTF("/badge.glb");
   const { nodes, materials } = gltf as unknown as CustomGLTFResult;
 
-  const texture = useTexture(
-    process.env.NODE_ENV === "development"
-      ? "http://localhost:3000/band.png"
-      : "https://dakshie.xyz/band.png"
+  const texture = useTexture("/band.png"
   );
   const { width, height } = useThree((state) => state.size);
   const [curve] = useState(
